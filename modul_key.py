0a8c148703e593e09b795316daff1acb3aeca032
fevzifevziev <69086204+fevzifevziev@users.noreply.github.com>
2020-11-14 01:43:00 +0300

    Update modul_key.py

--- modul_key.py
+++ modul_key.py
@@ -1,6 +1,8 @@
 import random
 import modul_1
 import mod_peremen
+
+
 fail_kluch = open('ключ.txt', 'w', encoding='utf-8')     # Открываем файл "ключ.txt"
 A      = mod_peremen.Kluch()                           # Присваиваем переменной "A" двумерный список модуля "mod_peremen" функции "Kluch()"
 
@@ -14,9 +16,3 @@ for i in range(len(A)):                          # Заполняем файл "
         fail_kluch.write(A[i][j])
     fail_kluch.write("\n")
 fail_kluch.close()                               # Закрываем файл  "ключ.txt"
-
-for i in range(len(A)):
-    for j in range(len(A[i])):
-        print(A[i][j], end=' ')
-    print("")
-print("\n")
